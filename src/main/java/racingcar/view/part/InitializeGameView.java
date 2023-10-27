43bc31f82b2711815c8d7865c21437d45af30b46
JaehongDev <println@kakao.com>
2023-10-27 19:48:24 +0900

    feat: InitializeGameView 구현

---
+++ src/main/java/racingcar/view/part/InitializeGameView.java
@@ -0,0 +1,18 @@
+package racingcar.view.part;
+
+import racingcar.event.EventListener;
+import racingcar.event.part.InitializeParticipantsEvent;
+import racingcar.event.part.InitializeTrackCountEvent;
+import racingcar.view.core.InputView;
+import racingcar.view.core.ViewContainer.DefaultView;
+
+public record InitializeGameView(InputView inputView, EventListener eventListener) implements DefaultView {
+    @Override
+    public void open() {
+        eventListener.listenWithParameter(InitializeParticipantsEvent::new)
+                .accept(inputView.readParticipants());
+
+        eventListener.listenWithParameter(InitializeTrackCountEvent::new)
+                .accept(inputView.readTrackCount());
+    }
+}
