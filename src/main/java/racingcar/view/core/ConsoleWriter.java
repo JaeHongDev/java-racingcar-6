889a9b4cd4fd89382b1c5bfa9ed7dc2585c1ebe8
JaehongDev <println@kakao.com>
2023-10-27 19:11:52 +0900

    feat: InputView & OutputView 입출력 메시지 정의

---
+++ src/main/java/racingcar/view/core/ConsoleWriter.java
@@ -0,0 +1,12 @@
+package racingcar.view.core;
+
+public sealed class ConsoleWriter permits InputView, OutputView {
+
+    protected void print(final Object data) {
+        System.out.print(data);
+    }
+
+    protected void println(final Object data) {
+        System.out.println(data);
+    }
+}
---
+++ src/main/java/racingcar/view/core/OutputView.java
@@ -0,0 +1,13 @@
+package racingcar.view.core;
+
+public final class OutputView extends ConsoleWriter {
+
+
+    public void printRacingResult() {
+        this.println("실행 결과");
+    }
+
+    public void printWinner() {
+        this.println("최종 우승자");
+    }
+}
