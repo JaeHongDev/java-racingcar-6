e830b98e442eb232c90766318e322aa8f098690b
JaehongDev <println@kakao.com>
2023-10-29 15:09:44 +0900

    feat: OutputView 레이스 결과 출력 구현

--- src/main/java/racingcar/view/core/OutputView.java
+++ src/main/java/racingcar/view/core/OutputView.java
@@ -1,4 +1,10 @@
 package racingcar.view.core;
 
+import racingcar.model.TrackPerResultModel;
+
 public final class OutputView extends ConsoleWriter {
+    public void printTrackPerResult(TrackPerResultModel trackPerResultModel) {
+        this.println(trackPerResultModel.toResult());
+        this.println("");
+    }
 }
